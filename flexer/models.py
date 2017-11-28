1f048a42ecb1bf2c1835d4daa0a498bd00217eeb
Teemu Salminen <salminen.th@gmail.com>
2017-11-28 18:08:45 +0200

    Add database migrations

--- flexer/models.py
+++ flexer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=40, blank=False)
+    first_name = models.CharField(max_length=20, blank=False)
+    last_name = models.CharField(max_length=20, blank=False)
+    password = models.CharField(max_length=30, blank=False)
+    def __str__(self):
+        return '%s, %s, %s' % (self.email, self.first_name, self.last_name)
--- flexerserver/settings.py
+++ flexerserver/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'flexer',
+    'flexer.apps.FlexerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
